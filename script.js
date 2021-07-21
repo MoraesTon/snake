ccdabbb7761ff93baa1ac253a532667051a0f408
tonMoraes <tondevspace@gmail.com>
2021-07-21 20:58:50 -0300

    set directions and avoid snake disapearing

--- script.js
+++ script.js
@@ -22,7 +22,22 @@ function createSnake() {
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function gameStart() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
+
     createBG();
     createSnake();
 
@@ -32,7 +47,7 @@ function gameStart() {
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "donw") snakeY += box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
     let newHead = {
