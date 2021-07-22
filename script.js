43864febc3f1ef42fc174c7d5f94eb3642cfcb18
tonMoraes <tondevspace@gmail.com>
2021-07-21 21:06:06 -0300

    create food

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -20,6 +24,11 @@ function createSnake() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener("keydown", update);
@@ -40,6 +49,7 @@ function gameStart() {
 
     createBG();
     createSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
